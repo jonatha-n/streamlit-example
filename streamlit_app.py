e70465b698e4d625e3c2512a8fa9c1d3f40f863f
Jonathan <95885300+jonatha-n@users.noreply.github.com>
2023-11-15 23:57:59 +0000

    update 11/15

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Like-Song Finder
+# Title Placeholder
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
@@ -12,29 +12,16 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+df = pd.read_csv('spotify_songs_edit.csv')
+df = df.drop_duplicates(subset=['track_name'])
+df_names = pd.read_csv('non_numeric_data.csv')
+list_of_artists = df_names[['track_artist']].drop_duplicates()
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+box_disabled = True
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+input_artist_name = st.selectbox('Enter an artist name or band', list_of_artists)
+input_track_name = st.selectbox('Enter a track name', ['a', 'b', 'c'], disabled=box_disabled)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# df.loc[df['column_name'] == some_value]
+# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
