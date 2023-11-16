5b9844f82da606a3df90cf91acb865735c4028e7
Jonathan <95885300+jonatha-n@users.noreply.github.com>
2023-11-16 21:14:48 +0000

    11/16

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,23 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-
+from matplotlib import pyplot
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import euclidean_distances
 """
 # Title Placeholder
 """
-if "disabled" not in st.session_state:
-    st.session_state.disabled = True
-
-
-def callback():
-    st.session_state.disabled = False
 
 df = pd.read_csv('spotify_songs_edit.csv')
 df = df.drop_duplicates(subset=['track_name'])
-df_names = pd.read_csv('non_numeric_data.csv')
-list_of_artists = df_names[['track_artist']].drop_duplicates()
+df_tracks = pd.read_csv('non_numeric_data.csv')
+df_tracks = df_tracks[['track_artist', 'track_name']].drop_duplicates()
+df_tracks['track_and_artist'] = df_tracks['track_name'] + " --- by " + df_tracks['track_artist']
 
 box_disabled = True
 
-input_artist_name = st.selectbox('Enter an artist name or band', list_of_artists)
-
-
-input_track_name = st.selectbox('Enter a track name', ['a', 'b', 'c'], disabled=st.session_state.disabled, on_change=callback())
+input_artist_name = st.selectbox('Type a song name and make selection', df_tracks[['track_and_artist']], help="Not all artists are available. Please make a selection from the listed choices.")
 
 # df.loc[df['column_name'] == some_value]
 # num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
