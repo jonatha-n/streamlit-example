f1835dd7d81e6cc2f182b5f80f9a85604f7206a8
Jonathan <95885300+jonatha-n@users.noreply.github.com>
2023-11-16 04:08:12 +0000

    updates night time

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,14 @@ import streamlit as st
 
 """
 # Title Placeholder
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
+if "disabled" not in st.session_state:
+    st.session_state.disabled = True
+
+
+def callback():
+    st.session_state.disabled = False
+
 df = pd.read_csv('spotify_songs_edit.csv')
 df = df.drop_duplicates(subset=['track_name'])
 df_names = pd.read_csv('non_numeric_data.csv')
@@ -20,7 +21,9 @@ list_of_artists = df_names[['track_artist']].drop_duplicates()
 box_disabled = True
 
 input_artist_name = st.selectbox('Enter an artist name or band', list_of_artists)
-input_track_name = st.selectbox('Enter a track name', ['a', 'b', 'c'], disabled=box_disabled)
+
+
+input_track_name = st.selectbox('Enter a track name', ['a', 'b', 'c'], disabled=st.session_state.disabled, on_change=callback())
 
 # df.loc[df['column_name'] == some_value]
 # num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
