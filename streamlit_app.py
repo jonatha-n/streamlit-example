4330e3b5d5f007d567e1f3eb73211aa598e6b0b4
Jonathan <95885300+jonatha-n@users.noreply.github.com>
2023-11-17 00:54:09 +0000

    dsdsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,27 @@ from sklearn.metrics import euclidean_distances
 # Title Placeholder
 """
 
-df = pd.read_csv('spotify_songs_edit.csv')
+# Read in all data
+df = pd.read_csv('spotify_songs_edit_2.csv')
 df = df.drop_duplicates(subset=['track_name'])
+
+# Read in non-numeric data for use in User-Interface
 df_tracks = pd.read_csv('non_numeric_data.csv')
 df_tracks = df_tracks[['track_artist', 'track_name']].drop_duplicates()
 df_tracks['track_and_artist'] = df_tracks['track_name'] + " --- by " + df_tracks['track_artist']
 
-box_disabled = True
-
+# Select Box
 input_artist_name = st.selectbox('Type a song name and make selection', df_tracks[['track_and_artist']], help="Not all artists are available. Please make a selection from the listed choices.")
 
+# Attributes that will be used to create clusters
+att = df[['energy', 'key', 'valence', 'acousticness', 'speechiness']]
+scaler = StandardScaler()
+attributes_scaled = scaler.fit_transform(att)
+num_clusters = 6
+
+# Use KMeans to determine clusters
+kmeans = KMeans(n_clusters=num_clusters, random_state=0)
+df['cluster'] = kmeans.fit_predict(attributes_scaled)
+df['cluster'] = df['cluster'].astype('category')
+
 # df.loc[df['column_name'] == some_value]
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
