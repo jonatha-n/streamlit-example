40aa468154e5b7a3f0ee680abb2f93a8bfda26c7
Jonathan <95885300+jonatha-n@users.noreply.github.com>
2023-12-05 22:02:49 +0000

    updaatteeyayy

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,10 @@ df_tracks = pd.read_csv('non_numeric_data.csv')
 df_tracks = df_tracks[['track_name', 'track_artist']].drop_duplicates()
 df_tracks['track_and_artist'] = df_tracks['track_name'] + " --- by " + df_tracks['track_artist']
 
+
+    
 # Select Box
-input_artist_name = st.selectbox('Type a song name and make selection', df_tracks[['track_and_artist']], help="Not all artists are available. Please make a selection from the listed choices.")
+input_artist_name = st.selectbox('Type a song name and make selection', df_tracks[['track_and_artist']], help="Not all songs are available. Please make a selection from the listed choices.", index=None)
 
 # Attributes that will be used to create clusters
 att = df[['energy', 'key', 'valence', 'acousticness', 'speechiness']]
@@ -36,37 +38,39 @@ df['cluster'] = kmeans.fit_predict(attributes_scaled)
 df['cluster'] = df['cluster'].astype('category')
 
 #Generate Similar songs
-st.button("Generate Similar Songs", type="primary")
-song_and_artist_list = input_artist_name.split(' --- by ')
+generated = st.button("Generate Similar Songs", type="primary")
 
-search = df.query('track_name == @song_and_artist_list[0] and track_artist == @song_and_artist_list[1]')
+if generated:
+    song_and_artist_list = input_artist_name.split(' --- by ')
 
-liked_song_att = search[['energy', 'key', 'valence', 'acousticness', 'speechiness']]
-liked_song_att_scaled = scaler.transform(liked_song_att)
-predict_cluster = kmeans.predict(liked_song_att_scaled)
-songs_in_same_cluster = df[df['cluster'] == predict_cluster[0]]
+    search = df.query('track_name == @song_and_artist_list[0] and track_artist == @song_and_artist_list[1]')
 
-songs_in_same_cluster_att = songs_in_same_cluster[['energy', 'key', 'valence', 'acousticness', 'speechiness']]
-songs_in_same_cluster_att_scaled = scaler.transform(songs_in_same_cluster_att)
-distances = euclidean_distances(liked_song_att_scaled, songs_in_same_cluster_att_scaled)
+    liked_song_att = search[['energy', 'key', 'valence', 'acousticness', 'speechiness']]
+    liked_song_att_scaled = scaler.transform(liked_song_att)
+    predict_cluster = kmeans.predict(liked_song_att_scaled)
+    songs_in_same_cluster = df[df['cluster'] == predict_cluster[0]]
 
-songs_in_same_cluster['distance_to_liked'] = distances[0]
-ranked_songs = songs_in_same_cluster.sort_values(by='distance_to_liked')
-top_ten_ranked = ranked_songs.iloc[1:11, [1,2,3,10]]
-top_ten_ranked['Ranked Similarity'] = range(1, 11)
-top_ten_ranked.set_index('Ranked Similarity', inplace=True)
-st.dataframe(top_ten_ranked, use_container_width=True, column_config={'track_name' : 'Track Name', 'track_artist' : 'Track Artist', 'playlist_genre' : 'Genre'})
+    songs_in_same_cluster_att = songs_in_same_cluster[['energy', 'key', 'valence', 'acousticness', 'speechiness']]
+    songs_in_same_cluster_att_scaled = scaler.transform(songs_in_same_cluster_att)
+    distances = euclidean_distances(liked_song_att_scaled, songs_in_same_cluster_att_scaled)
+
+    songs_in_same_cluster['distance_to_liked'] = distances[0]
+    ranked_songs = songs_in_same_cluster.sort_values(by='distance_to_liked')
+    top_ten_ranked = ranked_songs.iloc[1:11, [1,2,3,10]]
+    top_ten_ranked['Ranked Similarity'] = range(1, 11)
+    top_ten_ranked.set_index('Ranked Similarity', inplace=True)
+    st.dataframe(top_ten_ranked, use_container_width=True, column_config={'track_name' : 'Track Name', 'track_artist' : 'Track Artist', 'playlist_genre' : 'Genre'})
 # df.loc[df['column_name'] == some_value]
 
-# Display histograms
 # Make button actually work
 # Start with no selections
 
-
+st.write(" ")
+st.write(" ")
 """
-# Data Distribution
+# Data Distribution for Song Characteristics
 """
-fig1 = pyplot.figure(figsize=[10, 10])
+fig1 = pyplot.figure(figsize=[8, 15])
 ax1 = fig1.add_subplot(5,1,1)
 ax2 = fig1.add_subplot(5,1,2)
 ax3 = fig1.add_subplot(5,1,3)
@@ -75,37 +79,24 @@ ax5 = fig1.add_subplot(5,1,5)
 
 
 ax1.hist(df['energy'])
-ax1.set_xlabel('Energy')
+ax1.set_xlabel('Energy Values')
 ax1.set_ylabel('Frequency')
 
 ax2.hist(df['key'])
-ax2.set_xlabel('Key')
+ax2.set_xlabel('Key Values')
 ax2.set_ylabel('Frequency')
 
 ax3.hist(df['valence'])
-ax3.set_xlabel('Valence')
+ax3.set_xlabel('Valence Values')
 ax3.set_ylabel('Frequency')
 
 ax4.hist(df['acousticness'])
-ax4.set_xlabel('Acousticness')
+ax4.set_xlabel('Acousticness Values')
 ax4.set_ylabel('Frequency')
 
 ax5.hist(df['speechiness'])
-ax5.set_xlabel('Speechiness')
+ax5.set_xlabel('Speechiness Values')
 ax5.set_ylabel('Frequency')
 
-fig1.tight_layout(pad=1)
+fig1.tight_layout(pad=2)
 st.pyplot(fig1)
-
-#pyplot.hist(df['energy'])
-#pyplot.title('Histogram of Energy for All Songs')
-#pyplot.xlabel('Energy Value')
-#pyplot.ylabel('Frequency')
-
-
-#pyplot.hist(df['Key'])
-#pyplot.title('Histogram of Keys for All Songs')
-#pyplot.xlabel('Key')
-#pyplot.ylabel('Frequency')
-
-#st.pyplot()
